6b053a78c08679c840d03c406fb53683401fb205
MD Nabeel Ahemad <106942480+nabeel539@users.noreply.github.com>
2023-11-28 07:31:29 +0000

    While Loops

---
+++ 05_iterations/02_while_loop.js
@@ -0,0 +1,24 @@
+// While
+
+let i = 0;
+while (i<=10) {
+    console.log(`Value of index is ${i}`);
+    i = i + 2;
+}
+
+// loop through an Array
+let myArray = ["Flash", "Batman", "Superman"]
+let arr = 0
+while (arr <myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr++
+} 
+
+
+
+// Do- while Loop
+let score = 11;
+do {
+console.log(`Score is ${score}`);
+score++;
+} while (score <= 10);
