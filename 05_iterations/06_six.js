2a9f789aacb89e9ebd85ca560b86ee090c1f60a5
MD Nabeel Ahemad <106942480+nabeel539@users.noreply.github.com>
2023-11-29 05:45:41 +0000

    Reduce function

--- 05_iterations/06_six.js
+++ 05_iterations/06_six.js
@@ -7,3 +7,31 @@ const newNums = myNums
                 .map( (num) => num + 1)
                 .filter( (num) => num >= 40)
 console.log(newNums);
+
+
+// Reduce
+const integerNum = [1,2,3,4]
+
+const myTotal = integerNum.reduce( (acc, curr) => (acc + curr),0)
+console.log(myTotal);               // 10
+
+
+// Shopping cart 
+
+const shoppingCart = [
+    {
+        itemName : "Think & Grow Rich",
+        price : 349
+    },
+    {
+        itemName: "Rich Dad Poor Dad",
+        price : 849
+    },
+    {
+        itemName : "You can Sell",
+        price : 1749
+    }
+]
+
+let totalvalue =  shoppingCart.reduce( (acc, item) => acc + item.price,0)
+console.log(totalvalue);        // 2947
