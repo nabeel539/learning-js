f4cee5514e893c986ec04a2cfbc1549a9473b231
MD Nabeel Ahemad <106942480+nabeel539@users.noreply.github.com>
2023-11-30 10:14:45 +0000

    Asyn & Sync JS

---
+++ 09_Advance/async.js
@@ -0,0 +1,11 @@
+/* 
+
+Notes :-
+
+1. JS is a Synchronous & Single threaded language...(Default)
+
+2. Executes one line of code at a time.
+3. Blocking code (Sync)
+4. Non-Blocking Code (Async)
+
+*/
