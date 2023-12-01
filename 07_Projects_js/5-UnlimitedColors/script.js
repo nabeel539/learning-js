1c79268ae6129f95aaab7549b16fa3b7da6ae061
MD Nabeel Ahemad <106942480+nabeel539@users.noreply.github.com>
2023-12-01 10:40:03 +0000

    Unlimited Colors

---
+++ 07_Projects_js/5-UnlimitedColors/script.js
@@ -0,0 +1,31 @@
+// generate Random Color
+const randomColor = function(){
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+       color += hex[Math.floor(Math.random()*16)];
+  
+    }
+    return color;
+}
+
+// console.log(randomColor());
+
+let intervalId;
+const startChangingColor = function(){
+   if (!intervalId) {
+    intervalId = setInterval(changeBgColor,500);
+   }
+}
+
+const stopChangingColor = function(){
+    clearInterval(intervalId);
+    intervalId = null;
+}
+
+function changeBgColor() {
+    document.body.style.backgroundColor = randomColor();
+}
+
+document.querySelector("#start").addEventListener('click', startChangingColor);
+document.querySelector("#stop").addEventListener('click', stopChangingColor);
